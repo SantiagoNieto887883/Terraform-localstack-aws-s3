3f9cf9054ded15cae0f1adece56954e755e41f37
SantiagoNieto887883 <sant887883@gmail.com>
2026-03-26 21:00:29 -0500

    re-allocate version statements

--- IAC/Terraform/LIVE/DEV/provider.tf
+++ IAC/Terraform/LIVE/DEV/provider.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_version = ">= 1.6"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-  # SIN backend aquí (lo pasarás en terraform init -backend-config=...)
-  #backend "s3" {}
-}
 
 provider "aws" {
 #  region                      = "us-east-1"
